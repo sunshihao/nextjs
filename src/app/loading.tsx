7021bf595bbb5d5867108e79138cd4122cd6fff8
sssh <sunshihao945@outlook.com>
2023-02-12 08:48:15 +0800

    fix: loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import styles from "./loading.module.css";
+// 全局loading
+export default function Loading() {
+  return (
+    <div className={styles.loading_home}>
+      <div className={`${styles.loading_home_2} ${styles.loading_home_text}`}>
+        Loading...
+      </div>
+    </div>
+  );
+}
