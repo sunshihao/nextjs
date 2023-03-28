65f93937010e63e1c769311466ed9c9c5d41e3c3
SSSH <sunshihao945@outlook.com>
2023-03-28 17:29:28 +0800

    style: 样式调整

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -173,53 +173,31 @@ const Chat = () => {
         </form>
       </div>
       {/* 侧边栏 */}
-      <Modal show={drawerFlag} onClose={() => setDrawerFlag(false)}>
+      <Modal
+        show={drawerFlag}
+        position="center"
+        popup={false}
+        onClose={() => setDrawerFlag(false)}
+      >
         <div
-          className="fixed top-0 right-0 z-40 h-screen p-4 overflow-y-auto transition-transform translate-x-full bg-white w-80 dark:bg-gray-800"
+          className="h-screen p-4 overflow-y-auto transition-transform translate-x-full bg-white w-60 dark:bg-gray-800"
           aria-labelledby="drawer-right-label"
         >
-          <h5
-            id="drawer-right-label"
-            className="inline-flex items-center mb-4 text-base font-semibold text-gray-500 dark:text-gray-400"
-          >
-            功能区
-          </h5>
-          <button
-            type="button"
-            className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-            onClick={() => setDrawerFlag(false)}
-          >
-            <Imager
-              src="/svg/cancel.svg"
-              width={10}
-              height={10}
-              alt=""
-              style={{ width: "1.5em" }}
-            />
-            <span className="sr-only">Close menu</span>
-          </button>
+          <Modal.Header>CHAT KEY</Modal.Header>
+          <Modal.Body>
 
-          <div className="grid grid-cols-2 gap-4">
-            <a
-              href="#"
-              className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-            >
-              Get access
-              <svg
-                className="w-4 h-4 ml-2"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+            dsad
+          </Modal.Body>
+          <Modal.Footer>
+            <div className="grid grid-cols-2 gap-4">
+              <a
+                href="#"
+                className="flex w-24 justify-center items-center text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5"
               >
-                <path
-                  fill-rule="evenodd"
-                  d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </a>
-          </div>
+                确认
+              </a>
+            </div>
+          </Modal.Footer>
         </div>
       </Modal>
     </div>
